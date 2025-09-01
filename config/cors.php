dc1359b71535195f1f74143271d8d93307b45b27
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-09-02 04:13:42 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -24,7 +24,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
-        'https://rehab-app-seven.vercel.app/'
+        'https://rehab-app-seven.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
